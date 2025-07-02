0c6eae845c37445862bb6c772465d7007fc63e57
ayushudaydubey <102001ayushdubey@gmail.com>
2025-07-03 03:47:35 +0530

    vercel url

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express, { urlencoded } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from  './src/routes/userRoutes.js'
+import userRoutes from './src/routes/userRoutes.js';
 import rateLimiter from './src/middleware/rateLimiter.js';
 import cors from 'cors';
 import { connectDB } from './src/config/db.js';
@@ -9,10 +9,21 @@ import errorHandler from './src/middleware/errorHandler.js';
 dotenv.config();
 const app = express();
 
+
 connectDB();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://doubt-tracker-frontend.vercel.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error("Not allowed by CORS"));
+  },
   credentials: true,
 }));
 
@@ -21,14 +32,16 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(rateLimiter);
 
-app.use('/', userRoutes);
-// app.use('//doubts', doubtRoutes);
-// app.use('//comments', commentRoutes);
 
+app.use('/', userRoutes);
+// app.use('/doubts', doubtRoutes);
+// app.use('/comments', commentRoutes);
+
+// Error handler
 app.use(errorHandler);
 
 
-app.listen(3000,()=>{
-  connectDB()
-  console.log("Server is Running ")
-})
+
+app.listen(3000, () => {
+  console.log(`Server is running `);
+});
